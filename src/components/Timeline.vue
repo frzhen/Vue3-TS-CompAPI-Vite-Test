250abcdadceafcb1ee3cfbeb947e0a8604c483c2
Fred Zhen <fred.zhen@gmail.com>
2022-12-15 17:09:59 +0800

    Added v-bind, ref, and .prevent

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -4,15 +4,24 @@
   * @Email: fred.zhen@gmail.com
 -->
 <script setup lang="ts">
+import { ref } from "vue";
+
 const periods = ["Today", "This Week", "This Month"];
 
+let selectedPeriod = ref("Today");
+const selectPeriod = (period: string) => {
+  console.log(period);
+  selectedPeriod.value = period;
+}
 </script>
 
 <template>
   <nav class="is-primary panel">
+    {{ selectedPeriod }}
     <span class="panel-tabs">
       <a v-for="period of periods"
-        :key="period"
+         :key="period"
+         @click.prevent="selectPeriod(period)"
          href="">
         {{ period }}
       </a>
