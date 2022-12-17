f35589b4e4b8601b166aca417c38fa663f353a96
Fred Zhen <fred.zhen@gmail.com>
2022-12-17 12:14:31 +0800

    Extracted periods constant and Period interface to the constants.ts to reduce complexity of the Timeline component

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+/**
+ * @Author: Fred R. Zhen
+ * @Date: 2022/12/17 12:11
+ * @Email: fred.zhen@gmail.com
+ */
+
+// Using 'as const' and typeof eliminating repetition in code
+export const periods = ["Today", "This Week", "This Month"] as const;
+
+export type Period = typeof periods[number];
